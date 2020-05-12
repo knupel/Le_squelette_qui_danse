ebcd9aab7345216e5072b84fb68fdad1507529c3
Stan le Punk <stanislasiii@gmail.com>
2020-05-12 07:44:14 +0200

    remove unused method

--- src/App.js
+++ src/App.js
@@ -23,27 +23,4 @@ function App() {
   );
 }
 
-// export default App;
-
-function Machin() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Le Codid 19 c'est pas top!!!
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default Machin;
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Machin from './App';
+import App from './App';
 
 test('mon premier test', () => {
-  const { getByText } = render(<Machin />);
+  const { getByText } = render(<App />);
   const linkElement = getByText(/Le Codid 19 c'est pas top!!!/i);
   expect(linkElement).toBeInTheDocument();
 });
